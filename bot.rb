74655d7d635c950e2f0a3865b3dcb0a6f0994cfe
William Mathewson <wncmathewson@me.com>
2014-07-15 19:10:01 +0100

    And so it begins...

--- bot.rb
+++ bot.rb
@@ -1,10 +1,18 @@
 require 'cinch'
 
+require_relative 'plugins/yo'
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.user = 'neanb0t'
     c.server = '87.117.253.48'
     c.port = '31333'
+    c.password = 'Nemo me impune lacessit'
     c.channels = ['#postevolution w0rdsn0tw4r']
+
+    c.plugins.prefix = ''
+    c.plugins.plugins = [Yo]
   end
 end
+
+bot.start
