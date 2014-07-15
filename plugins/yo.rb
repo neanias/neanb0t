cbcb044f50f865048996cf4e39be6b536121aa21
William Mathewson <wncmathewson@me.com>
2014-07-15 19:08:28 +0100

    Test plugin that yos

---
+++ plugins/yo.rb
@@ -0,0 +1,15 @@
+require 'cinch'
+
+class Yo
+  include Cinch::Plugin
+
+  match /^!yo ?(.*)/i
+
+  def execute(msg, query)
+    if query.empty?
+      msg.reply "Yo, #{msg.user.nick}"
+    else
+      msg.reply "Yo, #{query}"
+    end
+  end
+end
