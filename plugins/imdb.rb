0f6a7dff76afa465b425d8640ba6afa3c0de0fe5
William Mathewson <wncmathewson@me.com>
2014-07-17 19:46:30 +0100

    Keys weren't being symbolised, so using strings

--- plugins/imdb.rb
+++ plugins/imdb.rb
@@ -19,8 +19,8 @@ class IMDb
       if search_failed? api_response
         msg.reply "#{query} couldn't be found."
       else
-        best_match = api_response[:Search][0]
-        imdb_record = get_data(best_match[:imdbID])
+        best_match = api_response['Search'][0]
+        imdb_record = get_data(best_match['imdbID'])
 
         format_film(msg, imdb_record)
       end
@@ -31,28 +31,28 @@ class IMDb
     escaped_query = URI.escape(query)
     raw_return = self.class.get("/?s=#{escaped_query}")
 
-    JSON.parse(raw_return, JSON_HASH)
+    JSON.parse(raw_return)
   end
 
   def search_failed?(hash)
-    (hash.has_key?(:Error) || hash[:Response] = "False")
+    (hash.has_key?('Error') || hash.has_key?('Response'))
   end
 
   def get_data(imdb_id)
     raw_json = self.class.get("/?i=#{imdb_id}")
 
-    JSON.parse(raw_json, JSON_HASH)
+    JSON.parse(raw_json)
   end
 
   def format_film(msg, film_hash)
-    msg.reply "#{film_hash[:Title]} (#{film_hash[:Year]})"
-    if film_hash[:Metascore] && film_hash[:imdbRating]
-      msg.reply "Metascore:   #{film_hash[:Metascore]}"
-      msg.reply "IMDb rating: #{film_hash[:imdbRating]}"
-    elsif film_hash[:Metascore] && !film_hash[:imdbRating]
-      msg.reply "Metascore: #{film_hash[:Metascore]}"
-    elsif film_hash[:imdbRating] && !film_hash[:Metascore]
-      msg.reply "IMDb rating: #{film_hash[:imdbRating]}"
+    msg.reply "#{film_hash['Title']} (#{film_hash['Year']})"
+    if film_hash['Metascore'] && film_hash['imdbRating']
+      msg.reply "Metascore:   #{film_hash['Metascore']}"
+      msg.reply "IMDb rating: #{film_hash['imdbRating']}"
+    elsif film_hash['Metascore'] && !film_hash['imdbRating']
+      msg.reply "Metascore: #{film_hash['Metascore']}"
+    elsif film_hash['imdbRating'] && !film_hash['Metascore']
+      msg.reply "IMDb rating: #{film_hash['imdbRating']}"
     end
   end
 end
