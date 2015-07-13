a61b222cb18be9892d9acae71bb66aeba05fccdd
William Mathewson <wncmathewson@me.com>
2015-07-13 19:32:04 +0100

    Add Yo and Giphy API stuff

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
 module BotConfig
-  BASE_DIR = '/home/will/Programming/neanb0t'
+  BASE_DIR  = '/home/will/Programming/neanb0t'
+  YO_API_TOKEN = ''
+
+  GIPHY_API_VERSION = 'v1'
+  GIPHY_API_KEY = 'dc6zaTOxFJmzC'
 end
