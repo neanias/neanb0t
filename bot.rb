a22bc2200a98de5886f0975fcd2836be530d583a
William Mathewson <wncmathewson@me.com>
2015-07-14 19:39:17 +0100

    Finish bot.rb for the moment

--- bot.rb
+++ bot.rb
@@ -2,45 +2,29 @@ require 'cinch'
 
 require_relative 'config'
 
-require_relative 'plugins/yo'
-require_relative 'plugins/fuck'
-require_relative 'plugins/gyf'
-require_relative 'plugins/karma'
-require_relative 'plugins/title'
-require_relative 'plugins/man_page'
-require_relative 'plugins/imdb'
-require_relative 'plugins/downloader'
-require_relative 'plugins/giphy'
+Dir['plugins/**'].each do |pl|
+    require_relative pl
+end
 
-efnet = Cinch::Bot.new do
+Giphy::Configuration.configure do |config|
+  config.version = BotConfig::GIPHY_API_VERSION
+  config.api_key = BotConfig::GIPHY_API_KEY
+end
+
+bot = Cinch::Bot.new do
   configure do |c|
-    c.user            = 'neanb0t/efnet'
-    c.nick            = 'neanb0t'
-    c.server          = '87.117.253.48'
-    c.port            = '31333'
-    c.password        = 'Nemo me impune lacessit'
-    c.channels        = ['#postevolution w0rdsn0tw4r']
+    c.user = BotConfig::BOT_USERNAME
+    c.nick = BotConfig::BOT_NICK
+    c.server = BotConfig::SERVER_URL
+    c.port = BotConfig::SERVER_PORT
+    c.password = BotConfig::SERVER_PASSWORD
 
-    c.plugins.prefix  = ''
-    c.plugins.plugins = [Yo, Fuck, Gyf, Karma, Title,
-                         ManPage, IMDb, FileDownloader, GiphyPlugin]
+    c.channels = BotConfig::NETWORK_CHANNELS
+
+    c.plugins.prefix = BotConfig::PLUGIN_PREFIX
+    c.plugins.plugins = [YoYo, Fuck, Gyf, Karma, Title, Butts, Sudo,
+                         ManPage, IMDb, FileDownloader, GiphyPlugin, Woodo]
   end
 end
 
-imaginarynet = Cinch::Bot.new do
-  configure do |c|
-    c.user = 'neanb0t/imaginarynet'
-    c.nick = 'neanb0t'
-    c.server = '87.117.253.48'
-    c.port = '31334'
-    c.password = 'Nemo me impune lacessit'
-    c.channels = ['#inf1']
-
-    c.plugins.prefix = ''
-    c.plugins.plugins = [Yo, Fuck, Gyf, Karma, Title,
-                         ManPage, IMDb, FileDownloader, GiphyPlugin]
-  end
-end
-
-efnet.start
-imaginarynet.start
+bot.start
